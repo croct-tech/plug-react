472c5fdeb4bf1eefbd2484e742a3549ed09f5cd2
tiagobarros01 <tbarros1337@hotmail.com>
2022-06-17 08:58:09 -0300

    refactor: Applies requested changes

--- src/hooks/useCroct.ssr.test.tsx
+++ src/hooks/useCroct.ssr.test.tsx
@@ -1,16 +1,11 @@
 import {renderHook} from '@testing-library/react-hooks/server';
-import {ReactNode} from 'react';
 import {Plug} from '@croct/plug';
 import {useCroct} from './useCroct';
-import {CroctProvider} from '../CroctProvider';
-
-type RenderHookProps = {
-    children: ReactNode,
-};
+import {CroctProvider, CroctProviderProps} from '../CroctProvider';
 
 describe('useCroct', () => {
     it('should not fail on server-side rendering', () => {
-        const {result} = renderHook<RenderHookProps, Plug>(() => useCroct(), {
+        const {result} = renderHook<CroctProviderProps, Plug>(() => useCroct(), {
             wrapper: ({children}) => (
                 <CroctProvider appId="00000000-0000-0000-0000-000000000000">
                     {children}
--- src/hooks/useCroct.test.tsx
+++ src/hooks/useCroct.test.tsx
@@ -1,18 +1,13 @@
 import {renderHook} from '@testing-library/react-hooks';
 import croct, {Plug} from '@croct/plug';
-import {ReactNode} from 'react';
 import {useCroct} from './useCroct';
-import {CroctContext} from '../CroctProvider';
-
-type RenderHookProps = {
-    children: ReactNode,
-};
+import {CroctContext, CroctProviderProps} from '../CroctProvider';
 
 describe('useCroct', () => {
     it('should fail if used out of the <CroctProvider/> component', () => {
         jest.spyOn(console, 'error').mockImplementation();
 
-        const {result} = renderHook<RenderHookProps, Plug>(() => useCroct());
+        const {result} = renderHook<CroctProviderProps, Plug>(() => useCroct());
 
         expect(console.error).toBeCalled();
 
@@ -21,7 +16,7 @@ describe('useCroct', () => {
     });
 
     it('should return the Plug instance', () => {
-        const {result} = renderHook<RenderHookProps, Plug>(() => useCroct(), {
+        const {result} = renderHook<CroctProviderProps, Plug>(() => useCroct(), {
             wrapper: ({children}) => (
                 <CroctContext.Provider value={{plug: croct}}>{children}</CroctContext.Provider>
             ),
