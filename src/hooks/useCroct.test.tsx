fef12c3e58816bf12ece88910329d46d127ba965
Koemy <francine.pns@gmail.com>
2022-10-05 09:06:47 -0300

    Fix test error

--- src/hooks/useCroct.test.tsx
+++ src/hooks/useCroct.test.tsx
@@ -1,28 +1,34 @@
-import {renderHook} from '@testing-library/react-hooks';
+import {FunctionComponent} from 'react';
+import {renderHook} from '@testing-library/react';
 import croct, {Plug} from '@croct/plug';
 import {useCroct} from './useCroct';
 import {CroctContext, CroctProviderProps} from '../CroctProvider';
 
 describe('useCroct', () => {
     it('should fail if used out of the <CroctProvider/> component', () => {
-        jest.spyOn(console, 'error').mockImplementation();
+        jest.spyOn(console, 'error')
+            .mockImplementation();
 
-        const {result} = renderHook<CroctProviderProps, Plug>(() => useCroct());
+        // https://reactjs.org/docs/error-boundaries.html#how-about-trycatch
+        try {
+            renderHook<Plug, FunctionComponent<CroctProviderProps>>(() => useCroct());
+        } catch (e: any) {
+            expect(console.error)
+                .toHaveBeenCalled();
 
-        // eslint-disable-next-line no-console
-        expect(console.error).toHaveBeenCalled();
-
-        expect(result.error?.message)
-            .toBe('useCroct() can only be used in the context of a <CroctProvider> component.');
+            expect(e.message)
+                .toBe('useCroct() can only be used in the context of a <CroctProvider> component.');
+        }
     });
 
     it('should return the Plug instance', () => {
-        const {result} = renderHook<CroctProviderProps, Plug>(() => useCroct(), {
+        const {result} = renderHook<Plug, FunctionComponent<CroctProviderProps>>(() => useCroct(), {
             wrapper: ({children}) => (
                 <CroctContext.Provider value={{plug: croct}}>{children}</CroctContext.Provider>
             ),
         });
 
-        expect(result.current).toBe(croct);
+        expect(result.current)
+            .toBe(croct);
     });
 });
