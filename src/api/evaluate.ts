d3db61ab717329c553d177038cf2dc25f8cece87
Marcos Passos <marcos@croct.com>
2022-12-27 11:50:47 -0300

    Add support for configuring a base endpoint URL

--- src/api/evaluate.ts
+++ src/api/evaluate.ts
@@ -3,12 +3,13 @@ import {JsonValue} from '@croct/plug/sdk/json';
 
 export type EvaluationOptions<T extends JsonValue = JsonValue> = BaseOptions & {
     apiKey: string,
+    baseEndpointUrl?: string,
     fallback?: T,
 };
 
 export function evaluate<T extends JsonValue>(query: string, options: EvaluationOptions<T>): Promise<T> {
-    const {apiKey, fallback, ...rest} = options;
-    const promise = (new Evaluator({apiKey: apiKey}))
+    const {apiKey, fallback, baseEndpointUrl, ...rest} = options;
+    const promise = (new Evaluator({apiKey: apiKey, baseEndpointUrl: baseEndpointUrl}))
         .evaluate(query, rest) as Promise<T>;
 
     if (fallback !== undefined) {
