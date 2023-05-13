33920571becf7b584aedbd81cd12dd95509bf6cb
Wellerson <wllrsn.c@gmail.com>
2023-05-13 14:46:07 -0300

    refactor: remove experimental config

--- examples/next-ssr-app/next.config.js
+++ examples/next-ssr-app/next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    experimental: {appDir: true},
-    env: {
-      CLIENT_ID_COOKIE: process.env.CLIENT_ID_COOKIE,
-      CROCT_API_KEY: process.env.CROCT_API_KEY,
-      NEXT_PUBLIC_CROCT_APP_ID: process.env.NEXT_PUBLIC_CROCT_APP_ID,
-    }
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    CLIENT_ID_COOKIE: process.env.CLIENT_ID_COOKIE,
+    CROCT_API_KEY: process.env.CROCT_API_KEY,
+    NEXT_PUBLIC_CROCT_APP_ID: process.env.NEXT_PUBLIC_CROCT_APP_ID,
+  },
 };
 
 module.exports = nextConfig;
