b7033cf831995d5a23d195d17a6f1624f3b6f00c
Wellerson <wllrsn.c@gmail.com>
2023-05-13 14:44:31 -0300

    feat: export env config

--- examples/next-ssr-app/next.config.js
+++ examples/next-ssr-app/next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     experimental: {appDir: true},
+    env: {
+      CLIENT_ID_COOKIE: process.env.CLIENT_ID_COOKIE,
+      CROCT_API_KEY: process.env.CROCT_API_KEY,
+      NEXT_PUBLIC_CROCT_APP_ID: process.env.NEXT_PUBLIC_CROCT_APP_ID,
+    }
 };
 
 module.exports = nextConfig;
